75cd28fd5f9769245556672f70eed59ff49f8acf
PastaAddict <kristitsope@gmail.com>
2023-04-14 02:19:48 -0400

    added snapshot ensembling

--- src/model.py
+++ src/model.py
@@ -3,7 +3,7 @@ import torchvision
 import torch
 
 
-def load_model(model_name = 'resnet50', freeze_counter=8):
+def load_model(model_name = 'resnet50', freeze_counter=7):
     if model_name == 'resnet50':
         model = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
 
@@ -13,14 +13,13 @@ def load_model(model_name = 'resnet50', freeze_counter=8):
     elif model_name == 'inception':
         model = models.inception_v3(pretrained=True)
 
-    
     for c, child in enumerate(model.children()):
-            if c<freeze_counter:
-                for param in child.parameters():
-                    param.requires_grad = False
+        if c<freeze_counter:
+            for param in child.parameters():
+                param.requires_grad = False
 
     num_ftrs = model.fc.in_features
-    '''
+    
     model.fc = torch.nn.Sequential(
         torch.nn.Linear(num_ftrs, 256),
         torch.nn.ReLU(),
@@ -35,6 +34,7 @@ def load_model(model_name = 'resnet50', freeze_counter=8):
         torch.nn.Dropout(0.2),
         torch.nn.Linear(1200, 12)
     )
+    '''
 
     return model
 
